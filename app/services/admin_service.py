19dca9cf8fab692ea88e631721ff782cefc18fcc
Arnorld Omoja <omojaarnorld7@gmail.com>
2026-05-05 11:07:11 +0300

    admin_service to filtering 9 counties and their wards

--- app/services/admin_service.py
+++ app/services/admin_service.py
@@ -2,10 +2,40 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 
+TARGET_COUNTIES = [
+    "NAIROBI",
+    "NAKURU",
+    "KISUMU",
+    "MOMBASA",
+    "KISII",
+    "NYERI",
+    "TANA RIVER",
+    "TAITA TAVETA",
+    "NAROK"
+]
 
 # -------------------------
 # COUNTIES
 # -------------------------
+# def get_counties(db: Session):
+#     query = """
+#     SELECT
+#         id,
+#         name,
+#         ST_AsGeoJSON(geometry) as geojson
+#     FROM admin_county
+#     """
+
+#     result = db.execute(text(query))
+
+#     return [
+#         {
+#             "id": row.id,
+#             "name": row.name,
+#             "geometry": row.geojson
+#         }
+#         for row in result
+#     ]
 def get_counties(db: Session):
     query = """
     SELECT
@@ -13,9 +43,13 @@ def get_counties(db: Session):
         name,
         ST_AsGeoJSON(geometry) as geojson
     FROM admin_county
+    WHERE UPPER(name) = ANY(:county_names)
     """
 
-    result = db.execute(text(query))
+    result = db.execute(
+        text(query),
+        {"county_names": TARGET_COUNTIES}
+    )
 
     return [
         {
@@ -26,21 +60,46 @@ def get_counties(db: Session):
         for row in result
     ]
 
-
 # -------------------------
 # SUBCOUNTIES
 # -------------------------
+# def get_subcounties(db: Session):
+#     query = """
+#     SELECT
+#         id,
+#         name,
+#         county_id,
+#         ST_AsGeoJSON(geometry) as geojson
+#     FROM admin_subcounty
+#     """
+
+#     result = db.execute(text(query))
+
+#     return [
+#         {
+#             "id": row.id,
+#             "name": row.name,
+#             "county_id": row.county_id,
+#             "geometry": row.geojson
+#         }
+#         for row in result
+#     ]
 def get_subcounties(db: Session):
     query = """
     SELECT
-        id,
-        name,
-        county_id,
-        ST_AsGeoJSON(geometry) as geojson
-    FROM admin_subcounty
+        sc.id,
+        sc.name,
+        sc.county_id,
+        ST_AsGeoJSON(sc.geometry) as geojson
+    FROM admin_subcounty sc
+    JOIN admin_county c ON sc.county_id = c.id
+    WHERE UPPER(c.name) = ANY(:county_names)
     """
 
-    result = db.execute(text(query))
+    result = db.execute(
+        text(query),
+        {"county_names": TARGET_COUNTIES}
+    )
 
     return [
         {
@@ -53,18 +112,48 @@ def get_subcounties(db: Session):
     ]
 
 
+# def get_subcounties_by_county(db: Session, county_id: str):
+#     query = """
+#     SELECT 
+#         id,
+#         name,
+#         county_id,
+#         ST_AsGeoJSON(geometry) as geojson
+#     FROM admin_subcounty
+#     WHERE county_id = :county_id
+#     """
+
+#     result = db.execute(text(query), {"county_id": county_id})
+
+#     return [
+#         {
+#             "id": row.id,
+#             "name": row.name,
+#             "county_id": row.county_id,
+#             "geometry": row.geojson
+#         }
+#         for row in result
+#     ]
 def get_subcounties_by_county(db: Session, county_id: str):
     query = """
     SELECT 
-        id,
-        name,
-        county_id,
-        ST_AsGeoJSON(geometry) as geojson
-    FROM admin_subcounty
-    WHERE county_id = :county_id
+        sc.id,
+        sc.name,
+        sc.county_id,
+        ST_AsGeoJSON(sc.geometry) as geojson
+    FROM admin_subcounty sc
+    JOIN admin_county c ON sc.county_id = c.id
+    WHERE sc.county_id = :county_id
+    AND UPPER(c.name) = ANY(:county_names)
     """
 
-    result = db.execute(text(query), {"county_id": county_id})
+    result = db.execute(
+        text(query),
+        {
+            "county_id": county_id,
+            "county_names": TARGET_COUNTIES
+        }
+    )
 
     return [
         {
@@ -80,18 +169,46 @@ def get_subcounties_by_county(db: Session, county_id: str):
 # -------------------------
 # WARDS
 # -------------------------
+# def get_wards(db: Session):
+#     query = """
+#     SELECT
+#         id,
+#         name,
+#         county_id,
+#         subcounty_id,
+#         ST_AsGeoJSON(geometry) as geojson
+#     FROM admin_ward
+#     """
+
+#     result = db.execute(text(query))
+
+#     return [
+#         {
+#             "id": row.id,
+#             "name": row.name,
+#             "county_id": row.county_id,
+#             "subcounty_id": row.subcounty_id,
+#             "geometry": row.geojson
+#         }
+#         for row in result
+#     ]
 def get_wards(db: Session):
     query = """
     SELECT
-        id,
-        name,
-        county_id,
-        subcounty_id,
-        ST_AsGeoJSON(geometry) as geojson
-    FROM admin_ward
+        w.id,
+        w.name,
+        w.county_id,
+        w.subcounty_id,
+        ST_AsGeoJSON(w.geometry) as geojson
+    FROM admin_ward w
+    JOIN admin_county c ON w.county_id = c.id
+    WHERE UPPER(c.name) = ANY(:county_names)
     """
 
-    result = db.execute(text(query))
+    result = db.execute(
+        text(query),
+        {"county_names": TARGET_COUNTIES}
+    )
 
     return [
         {
@@ -104,20 +221,51 @@ def get_wards(db: Session):
         for row in result
     ]
 
+# def get_wards_by_county(db: Session, county_id: str):
+#     query = """
+#     SELECT
+#         id,
+#         name,
+#         county_id,
+#         subcounty_id,
+#         ST_AsGeoJSON(geometry) as geojson
+#     FROM admin_ward
+#     WHERE county_id = :county_id
+#     """
 
+#     result = db.execute(text(query), {"county_id": county_id})
+
+#     return [
+#         {
+#             "id": row.id,
+#             "name": row.name,
+#             "county_id": row.county_id,
+#             "subcounty_id": row.subcounty_id,
+#             "geometry": row.geojson
+#         }
+#         for row in result
+#     ]
 def get_wards_by_county(db: Session, county_id: str):
     query = """
     SELECT
-        id,
-        name,
-        county_id,
-        subcounty_id,
-        ST_AsGeoJSON(geometry) as geojson
-    FROM admin_ward
-    WHERE county_id = :county_id
+        w.id,
+        w.name,
+        w.county_id,
+        w.subcounty_id,
+        ST_AsGeoJSON(w.geometry) as geojson
+    FROM admin_ward w
+    JOIN admin_county c ON w.county_id = c.id
+    WHERE w.county_id = :county_id
+    AND UPPER(c.name) = ANY(:county_names)
     """
 
-    result = db.execute(text(query), {"county_id": county_id})
+    result = db.execute(
+        text(query),
+        {
+            "county_id": county_id,
+            "county_names": TARGET_COUNTIES
+        }
+    )
 
     return [
         {
@@ -131,19 +279,51 @@ def get_wards_by_county(db: Session, county_id: str):
     ]
 
 
+# def get_wards_by_subcounty(db: Session, subcounty_id: str):
+#     query = """
+#     SELECT
+#         id,
+#         name,
+#         county_id,
+#         subcounty_id,
+#         ST_AsGeoJSON(geometry) as geojson
+#     FROM admin_ward
+#     WHERE subcounty_id = :subcounty_id
+#     """
+
+#     result = db.execute(text(query), {"subcounty_id": subcounty_id})
+
+#     return [
+#         {
+#             "id": row.id,
+#             "name": row.name,
+#             "county_id": row.county_id,
+#             "subcounty_id": row.subcounty_id,
+#             "geometry": row.geojson
+#         }
+#         for row in result
+#     ]
 def get_wards_by_subcounty(db: Session, subcounty_id: str):
     query = """
     SELECT
-        id,
-        name,
-        county_id,
-        subcounty_id,
-        ST_AsGeoJSON(geometry) as geojson
-    FROM admin_ward
-    WHERE subcounty_id = :subcounty_id
+        w.id,
+        w.name,
+        w.county_id,
+        w.subcounty_id,
+        ST_AsGeoJSON(w.geometry) as geojson
+    FROM admin_ward w
+    JOIN admin_county c ON w.county_id = c.id
+    WHERE w.subcounty_id = :subcounty_id
+    AND UPPER(c.name) = ANY(:county_names)
     """
 
-    result = db.execute(text(query), {"subcounty_id": subcounty_id})
+    result = db.execute(
+        text(query),
+        {
+            "subcounty_id": subcounty_id,
+            "county_names": TARGET_COUNTIES
+        }
+    )
 
     return [
         {
